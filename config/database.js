a4bd3fb8953fa79a16007e1debbccd166002f246
bryan vera <bveracachay@gmail.com>
2022-09-30 23:32:08 -0500

    se quita el console log

--- config/database.js
+++ config/database.js
@@ -4,8 +4,6 @@ const _config = require("../config/config.js");
 
 const env = process.env.ENV;
 const config = _config[env];
-console.log("env: ", env);
-console.log("config: ", config);
 // create connection
 env === "development"
   ? (db = new Sequelize(config.database, config.username, config.password, {
